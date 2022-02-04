13617c5a9ba5e75c8b351bfda830dbb886f01d33
ArmandoPaulinoNeto <armandopaulinosilvaneto@gmail.com>
2022-02-04 15:07:21 -0300

    Inclusão da activity AddTeaskActivity e da viewBinding no gradle.

---
+++ app/src/main/java/br/com/isoftware/todolist/AddTaskActivity.kt
@@ -0,0 +1,11 @@
+package br.com.isoftware.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activite_add_task)
+    }
+}
