c4b1f2d1c7fda379a19335ec518d490603fbe06b
ArmandoPaulinoNeto <armandopaulinosilvaneto@gmail.com>
2022-02-04 21:43:34 -0300

    Concluído o layout recyclerView task.

--- app/src/main/java/br/com/isoftware/todolist/MainActivity.kt
+++ app/src/main/java/br/com/isoftware/todolist/MainActivity.kt
@@ -1,11 +1,20 @@
 package br.com.isoftware.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.isoftware.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonAddTask.setOnClickListener {
+            startActivity(Intent(this, AddTaskActivity::class.java))
+        }
     }
 }
